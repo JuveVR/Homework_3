5798cd4f0a8a5694178ee9202de68b6d4baf4990
Vadym Rovenko <rovenkovad@gmail.com>
2019-02-22 14:31:36 +0200

    Made improvements and fixed mistakes regarding review.

--- Exercise_6.py
+++ Exercise_6.py
@@ -8,6 +8,8 @@ def is_year_leap(year):
 
 #6.2
 def triangle_existance(a,b,c):
+    #Checks if the sum of any two arguments is bigger then the third one.
+    #If yes returns True? if no - False.
     if a < b + c and b < c + a and c < b + a:
         return True
     else:
@@ -15,20 +17,51 @@ def triangle_existance(a,b,c):
 
 #6.3
 def spaceless():
+    #Asks to enter the word without spaces, spaces in the beginning and the end are allowed.
+    #Function works until conditions are met and returns entered word.
     while True:
         message = input("Enter message: ").strip()
         if " " not in message:
             return(message)
-            break
 
 #6.4
 def number_request():
+    # Asks to enter the number until its done. Returns entered number.
     while True:
         num = input("Enter number: ")
         try:
             num = float(num)
             return num
-            break
         except:
             pass
+
+
+
+#6.5
+def triangle_type(a,b,c):
+    # Checks arguments correlation with triangle sides:
+    # if the sum of any two arguments is bigger then the third one returns "Isosceles triangle",
+    # If less - returns "Not a triangle"
+    # If all arguments are equal returns "Equilateral triangle"
+    if a == b == c:
+        return "Equilateral triangle"
+    elif a < b + c and b < c + a and c < b + a:
+        return "Isosceles triangle"
+    else:
+        return "Not a triangle"
+
+#6.6
+def distance(x1,y1,x2,y2):
+    point1 = (x1,y1)
+    point2 = (x2,y2)
+
+
+
+# a = -7
+# b = -4
+#
+# c = a * b
+# print(c)
+
+
 
