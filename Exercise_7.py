1aa62495bf5f2d70820c095b03fe1722a6c2dfbb
Vadym Rovenko <rovenkovad@gmail.com>
2019-02-22 19:25:53 +0200

    Finished ex. 7

---
+++ Exercise_7.py
@@ -0,0 +1,18 @@
+s = """We are not what we should be! 
+We are not what we need to be.
+But at least we are not what we used to be
+(Football Coach)"""
+s1 = s.split()
+print(len(s.split()))
+
+s2 = []
+for i in s1:
+    s2.append(i.strip('!.()'))
+
+print(s2)
+
+
+def sortByAlphabet(inputStr):
+    return inputStr[0]
+#print(s2.sort()) This function returns None, can't understand why
+print(sorted(s2))
